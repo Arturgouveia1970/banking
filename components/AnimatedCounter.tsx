fc82b4b498b70201ced1f8c5f8f34117b5d38823
Arturgouveia1970 <artgouveiag@gmail.com>
2024-07-04 19:59:00 +0100

    implement main layuot, sidebar and mobilenav.

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import CountUp from 'react-countup'
+
+function AnimatedCounter({ amount }: { amount: number }) {
+  return (
+    <div className='w-full'>
+        <CountUp 
+          duration={2}
+          decimals={2}
+          end={amount} 
+          decimal=","
+          prefix='$'  
+        />
+    </div>
+  )
+}
+
+export default AnimatedCounter
